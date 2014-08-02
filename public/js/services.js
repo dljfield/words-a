f839d12ce73a0a1574a30eaaa6c029dcbe27d3dc
David Field <david@thedebonair.net>
2014-08-02 16:28:07 +0100

    Fixed up some minor things.

--- public/js/services.js
+++ public/js/services.js
@@ -3,9 +3,5 @@ angular.module('words.services', ['ngResource'])
 .factory('PostService', ['$resource', function($resource) {
     return $resource('/posts/:id', {
         id: '@id'
-    }, {
-        get: {
-            method: 'GET'
-        }
     });
 }]);
