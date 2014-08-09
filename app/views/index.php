9db02f4ab5ca4ce2149264bc4e128c1e3cbf64fb
David Field <david@thedebonair.net>
2014-08-09 15:33:14 +0100

    Added angular-ui-router as a dependency and tested to make sure it works. Now the work begins to set up the actual routes.

--- app/views/index.php
+++ app/views/index.php
@@ -1,1 +1,24 @@
-This is the index for words.app and will provide the initial page for the Angular application
+<!DOCTYPE html>
+<html lang="en" ng-app="words">
+<head>
+    <meta charset="UTF-8">
+    <title>thedebonair/words</title>
+</head>
+<body>
+
+    <div ui-view="viewA"></div>
+    <div ui-view="viewB"></div>
+    <!-- Also a way to navigate -->
+    <a ui-sref="route1">Route 1</a>
+    <a ui-sref="route2">Route 2</a>
+
+
+    <!-- ANGULAR -->
+    <script src="js/bower_components/angular/angular.js"></script>
+    <script src="js/bower_components/angular-ui-router/release/angular-ui-router.js"></script>
+
+    <!-- WORDS.APP -->
+    <script src="js/words/app.js"></script>
+
+</body>
+</html>
